a7bc041cc78f43cae60402c6afdd307c729565aa
99p <poccori@gmail.com>
2022-04-10 15:18:37 +0900

    finish Chapter7

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/new'
-  root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'static_pages#home'
+
+  get '/home', to: 'static_pages#home'
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+
+  resources :users
+  get '/signup', to: 'users#new'
+
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,13 +1,10 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get users_index_url
+
+  test "should get new" do
+    get new_user_url
     assert_response :success
   end
 
-  test "should get new" do
-    get users_new_url
-    assert_response :success
-  end
 end
